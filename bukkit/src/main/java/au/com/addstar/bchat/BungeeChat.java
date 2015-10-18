fc0081f0655dd8f8666a852effe6367291f20c16
Schmoller <steven.schmoll@gmail.com>
2015-10-18 14:14:11 +1100

    Initial Commit

---
+++ bukkit/src/main/java/au/com/addstar/bchat/BungeeChat.java
@@ -0,0 +1,7 @@
+package au.com.addstar.bchat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BungeeChat extends JavaPlugin {
+
+}
---
+++ proxy/src/main/java/au/com/addstar/bchat/BungeeChat.java
@@ -0,0 +1,7 @@
+package au.com.addstar.bchat;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class BungeeChat extends Plugin {
+
+}
